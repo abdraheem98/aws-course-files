fcf5c536dacd5827c54c42914652e4e71363cbbb
abdraheem98 <abdulraheem.akv@gmail.com>
2024-06-18 20:15:23 +0530

    first commit

--- main.py
+++ main.py
@@ -1,9 +1,31 @@
-thisdict = {
-  "brand": "Ford",
-  "model": "Mustang",
-  "year": 1964
-}
+#nested class
 
-print(len(thisdict))
+class Zoo:
+  def __init__(self):
+    self.animals = []
+
+  def add_animals(self,name,species):
+    animal = self.Animal(name,species)
+    self.animals.append(animal)
+
+  class Animal:
+    def __init__(self,name,species):
+      self.name = name
+      self.species = species
+
+    def display_info(self):
+      print(f"Name: {self.name} and Species: {self.species}")
+
+my_zoo = Zoo()
+
+my_zoo.add_animals("Lion", "Mammal")
+
+my_zoo.add_animals("Snake", "Reptile")
+
+
+my_zoo.add_animals("Eagle", "Bird")
+
+for animal in my_zoo.animals:
+  animal.display_info()
 
     
